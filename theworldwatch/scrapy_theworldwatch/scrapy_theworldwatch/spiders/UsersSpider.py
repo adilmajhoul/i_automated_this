1a15ba9ab0ae8ce00fd3c972b7e310a3b4a766cd
Adil_Majhoul <itsadilhadid@gmail.com>
2024-06-20 17:08:40 +0100

    update

--- theworldwatch/scrapy_theworldwatch/scrapy_theworldwatch/spiders/UsersSpider.py
+++ theworldwatch/scrapy_theworldwatch/scrapy_theworldwatch/spiders/UsersSpider.py
@@ -1,3 +1,4 @@
+from curses import meta
 import scrapy
 
 
@@ -22,9 +23,11 @@ class UsersSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(UsersSpider, self).__init__(*args, **kwargs)
         self.last_video_url = self.read_last_video_url()
+        print("__________self.last_video_url: ", self.last_video_url)
         self.last_video_reached = False
         self.current_page = 1
         self.first_video_url = None
+        self.videos_scraped = 1
 
     def read_last_video_url(self):
         try:
@@ -38,36 +41,53 @@ class UsersSpider(scrapy.Spider):
             file.write(url)
 
     def get_video_users(self, response):
+        video_url = response.meta["video_url"]
+
         for user in response.css(".username"):
             yield {
-                "username": user.css("::text").get(),
+                "+++++++++++++username": user.css("::text").get(),
                 "profile_link": user.attrib.get("href", "None"),
+                "video_url": video_url,
             }
 
     def parse(self, response):
-        if self.current_page == 1:
+        print(f">>>>>>>>>>> PAGE {self.current_page} <<<<<<<<<<<<<")
+
+        if self.videos_scraped == 0:
             self.first_video_url = response.css(".item > a").attrib["href"]
             print("______ self.first_video_url: ", self.first_video_url)
 
         for video in response.css(".item"):
+            print(f">>>>>>>>>>> VIDEO {self.videos_scraped} <<<<<<<<<<<<<")
+
             video_url = video.css(".item > a").attrib["href"]
 
             # Check if the current video URL is the last video URL
-            if video_url == self.last_video_url:
+            if video_url == self.last_video_url or (
+                self.last_video_url is None and self.videos_scraped >= 8
+            ):
+                print("___________Last video reached!")
+
+                if self.first_video_url:
+                    self.write_last_video_url(self.first_video_url)
+
                 self.last_video_reached = True
                 return
 
-            # yield response.follow(video_url, self.get_video_users)
-            yield {
-                "title": video.css(".title::text").get(),
-                "poster": video.css(".video-uploader::text").get(),
-                "views": video.css(".amount-views::text").get(),
-                "comments": video.css(".tme::text").get(),
-                "length": video.css(".drtn::text").get(),
-                "link": video.css(".item > a").attrib["href"],
-            }
+            yield response.follow(
+                video_url, self.get_video_users, meta={"video_url": video_url}
+            )
+            self.videos_scraped += 1
+            # yield {
+            #     "+++++++++++++ title": video.css(".title::text").get(),
+            #     "poster": video.css(".video-uploader::text").get(),
+            #     "views": video.css(".amount-views::text").get(),
+            #     "comments": video.css(".tme::text").get(),
+            #     "length": video.css(".drtn::text").get(),
+            #     "link": video.css(".item > a").attrib["href"],
+            # }
 
-        if self.last_video_url is None and self.current_page >= 2:
+        if self.last_video_url is None and self.videos_scraped >= 8:
             if self.first_video_url:
                 self.write_last_video_url(self.first_video_url)
             return
